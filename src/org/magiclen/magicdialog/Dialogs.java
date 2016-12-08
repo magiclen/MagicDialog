8f0584a1680d78dde40f4535408858066f0c7eb5
Magic Len <len@magiclen.org>
2016-12-08 20:13:35 +0800

    Release 2.0.0

---
+++ src/org/magiclen/magicdialog/Dialogs.java
@@ -0,0 +1,378 @@
+/*
+ *
+ * Copyright 2015-2017 magiclen.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.magiclen.magicdialog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Font;
+import javafx.stage.Window;
+import org.magiclen.magicaudioplayer.AudioPlayer;
+
+/**
+ * 一般對話框設定類別，若要建立出對話框必須使用create方法。
+ *
+ * @author Magic Len
+ */
+public class Dialogs implements Cloneable {
+
+    // -----類別介面-----
+    /**
+     * 對話框的按鈕事件。
+     */
+    public static interface ButtonEvent {
+
+        public void onClick();
+    }
+
+    // -----類別列舉-----
+    /**
+     * 對話框的類型。
+     */
+    public static enum Type {
+
+        NONE, INFORMATION, WARNING, ERROR, QUESTION;
+    }
+
+    /**
+     * 對話框的音效。
+     */
+    public static enum Audio {
+
+        NONE, DEFAULT, BUBUBU, COCOCO, DIN, DING_H_DING_L, DING_L_DING_H, DINGDING, DUDU, DUDUDU, EMERGE, HASTY, LOSE, MILD, SLUMP, VICTORY;
+    }
+
+    // -----類別常數-----
+    private static final int FONT_SIZE = 16;
+
+    // -----類別方法-----
+    /**
+     * 建立對話框的設定檔。
+     *
+     * @return 傳回新的對話框設定
+     */
+    public static Dialogs create() {
+        return new Dialogs();
+    }
+
+    // -----物件常數-----
+    private final ArrayList<ButtonType> buttonList = new ArrayList<>();
+    private final ArrayList<ButtonEvent> buttonEventList = new ArrayList<>();
+
+    // -----物件變數-----
+    private Window owner;
+    private String title = "Title";
+    private String message = "Message";
+    private String header = null;
+    private double fontSize = FONT_SIZE;
+    private Type type = Type.INFORMATION;
+    private Audio audio = Audio.DEFAULT;
+
+    // -----建構子-----
+    /**
+     * 建構子，設為private，無法提供其他類別實體化。
+     */
+    private Dialogs() {
+
+    }
+
+    // -----物件方法-----
+    /**
+     * 建立對話框。
+     *
+     * @return 傳回對話框
+     */
+    public Dialog createDialog() {
+        final Font font = new Font(fontSize);
+        final AlertType alertType;
+        switch (type) {
+            case INFORMATION:
+                alertType = AlertType.INFORMATION;
+                break;
+            case QUESTION:
+                alertType = AlertType.CONFIRMATION;
+                break;
+            case ERROR:
+                alertType = AlertType.ERROR;
+                break;
+            case WARNING:
+                alertType = AlertType.WARNING;
+                break;
+            default:
+                alertType = AlertType.NONE;
+                break;
+        }
+        final Alert dialog = new Alert(alertType);
+        dialog.initOwner(owner);
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+        dialog.setContentText(message);
+
+        if (!buttonList.isEmpty()) {
+            dialog.getButtonTypes().setAll(buttonList);
+            dialog.getDialogPane().getChildren().stream().filter(node -> node instanceof ButtonBar).forEach(node -> {
+                final ButtonBar buttonBar = (ButtonBar) node;
+                final List<Button> buttonBarList = new ArrayList<>();
+                buttonBar.getButtons().stream().filter(insideNode -> insideNode instanceof Button).forEach(insideNode -> {
+                    final Button button = (Button) insideNode;
+                    button.setFont(font);
+                    buttonBarList.add(button);
+                });
+                final int buttonCount = buttonBarList.size();
+                for (int i = 0; i < buttonCount; ++i) {
+                    final int index = i;
+                    final ButtonEvent event = buttonEventList.get(index);
+                    if (event != null) {
+                        buttonBarList.get(index).setOnAction((e) -> {
+                            event.onClick();
+                        });
+                    }
+                }
+            });
+        } else {
+            dialog.getDialogPane().getChildren().stream().filter(node -> node instanceof ButtonBar).forEach(node -> {
+                final ButtonBar buttonBar = (ButtonBar) node;
+                buttonBar.getButtons().stream().filter(insideNode -> insideNode instanceof Button).forEach(insideNode -> {
+                    final Button button = (Button) insideNode;
+                    button.setFont(font);
+                });
+            });
+        }
+
+        //設定標籤文字大小與高度
+        dialog.getDialogPane().getChildren().stream().filter(node -> node instanceof Label).forEach(node -> {
+            final Label label = (Label) node;
+            label.setFont(font);
+            label.setMinHeight(Region.USE_PREF_SIZE);
+        });
+
+        //加入音效
+        dialog.setOnShown(e -> {
+            playAudio();
+        });
+
+        return dialog;
+    }
+
+    /**
+     * 顯示對話框並播放出音效，此方法之後的程式將會繼續執行，如果要取得對話框物件的參考需使用createDialog方法。
+     */
+    public void show() {
+        final Dialog dialog = createDialog();
+        dialog.show();
+    }
+
+    /**
+     * 顯示對話框並播放出音效，此方法之後的程式將會暫時停止執行，如果要取得對話框物件的參考需使用createDialog方法。
+     *
+     * @return 傳回使用者按下的按鈕，如果沒有按，回傳null
+     */
+    public ButtonType showAndWait() {
+        final Dialog dialog = createDialog();
+        final Optional<ButtonType> buttonTypeOpt = dialog.showAndWait();
+        if (buttonTypeOpt.isPresent()) {
+            return buttonTypeOpt.get();
+        }
+        return null;
+    }
+
+    /**
+     * 設定擁有對話框的Window。
+     *
+     * @param owner 傳入擁有對話框的Window
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs owner(final Window owner) {
+        this.owner = owner;
+        return this;
+    }
+
+    /**
+     * 設定對話框的標題。
+     *
+     * @param title 傳入對話框的標題
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs title(final String title) {
+        this.title = title;
+        return this;
+    }
+
+    /**
+     * 設定對話框的訊息。
+     *
+     * @param message 傳入對話框的訊息
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs message(final String message) {
+        this.message = message;
+        return this;
+    }
+
+    /**
+     * 設定對話框的訊息標題。
+     *
+     * @param header 傳入對話框的訊息標題
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs header(final String header) {
+        this.header = header;
+        return this;
+    }
+
+    /**
+     * 設定對話框的字體大小。
+     *
+     * @param fontSize 傳入對話框的字體大小
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs fontSize(final double fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
+
+    /**
+     * 設定對話框的類型，將會影響到對話框顯示出來的圖片。
+     *
+     * @param type 傳入對話框的類型
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs type(final Type type) {
+        if (type == null) {
+            this.type = Type.NONE;
+        } else {
+            this.type = type;
+        }
+        return this;
+    }
+
+    /**
+     * 設定對話框的音效，音效將在使用show或是showAndWait方法時播放出來。
+     *
+     * @param audio 傳入對話框的音效
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs audio(final Audio audio) {
+        if (audio == null) {
+            this.audio = Audio.NONE;
+        } else {
+            this.audio = audio;
+        }
+        return this;
+    }
+
+    /**
+     * 加入按鈕至對話框中。
+     *
+     * @param button 傳入按鈕
+     * @param event 傳入按鈕的事件
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs addButton(final ButtonType button, final ButtonEvent event) {
+        buttonList.add(button);
+        buttonEventList.add(event);
+        return this;
+    }
+
+    /**
+     * 加入按鈕至對話框中。
+     *
+     * @param text 傳入按鈕的文字
+     * @param event 傳入按鈕的事件
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs addButton(final String text, final ButtonEvent event) {
+        return addButton(new ButtonType(text), event);
+    }
+
+    /**
+     * 加入按鈕至對話框中。
+     *
+     * @param text 傳入按鈕的文字
+     * @param type 傳入按鈕的類型
+     * @param event 傳入按鈕的事件
+     * @return 再把對話框設定傳回
+     */
+    public Dialogs addButton(final String text, final ButtonData type, final ButtonEvent event) {
+        return addButton(new ButtonType(text, type), event);
+    }
+
+    /**
+     * 複製對話框設定。
+     *
+     * @return 傳回設定值一樣但是為不同物件的對話框設定
+     * @throws java.lang.CloneNotSupportedException
+     */
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        final Dialogs dialogs = Dialogs.create();
+        dialogs.audio(audio).fontSize(fontSize).owner(owner).message(message).header(header).title(title).type(type);
+        final int buttonCount = buttonList.size();
+        for (int i = 0; i < buttonCount; ++i) {
+            dialogs.addButton(buttonList.get(i), buttonEventList.get(i));
+        }
+        return dialogs;
+    }
+
+    /**
+     * 播放音效。
+     */
+    private void playAudio() {
+        final String audioFileName;
+        switch (audio) {
+            case NONE:
+                return;
+            case DEFAULT:
+                switch (type) {
+                    case INFORMATION:
+                        audioFileName = "mild.wav";
+                        break;
+                    case QUESTION:
+                        audioFileName = "dududu.wav";
+                        break;
+                    case ERROR:
+                        audioFileName = "hasty.wav";
+                        break;
+                    case WARNING:
+                        audioFileName = "din.wav";
+                        break;
+                    default:
+                        return;
+                }
+                break;
+            default:
+                audioFileName = audio.toString().toLowerCase().concat(".wav");
+                break;
+        }
+        try {
+            final AudioPlayer ap = AudioPlayer.createPlayer(getClass().getResource("/org/magiclen/magicdialog/".concat(audioFileName)));
+            ap.setAutoClose(true);
+            ap.play();
+        } catch (final Exception ex) {
+        }
+    }
+}
---
+++ src/org/magiclen/magicdialog/InputDialogs.java
@@ -0,0 +1,258 @@
+/*
+ *
+ * Copyright 2015-2017 magiclen.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.magiclen.magicdialog;
+
+import java.util.Optional;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Window;
+import org.magiclen.magicaudioplayer.AudioPlayer;
+
+/**
+ * 輸入對話框設定類別，若要建立出對話框必須使用create方法。
+ *
+ * @author Magic Len
+ */
+public class InputDialogs implements Cloneable {
+
+    // -----類別介面-----
+    // -----類別列舉-----
+    /**
+     * 對話框的音效。
+     */
+    public static enum Audio {
+
+        NONE, DEFAULT, BUBUBU, COCOCO, DIN, DING_H_DING_L, DING_L_DING_H, DINGDING, DUDU, DUDUDU, EMERGE, HASTY, LOSE, MILD, SLUMP, VICTORY;
+    }
+
+    // -----類別常數-----
+    private static final int FONT_SIZE = 16;
+
+    // -----類別方法-----
+    /**
+     * 建立對話框的設定檔。
+     *
+     * @return 傳回新的對話框設定
+     */
+    public static InputDialogs create() {
+        return new InputDialogs();
+    }
+
+    // -----物件變數-----
+    private Window owner;
+    private String title = "Title";
+    private String message = null;
+    private String header = null;
+    private String text = null;
+    private double fontSize = FONT_SIZE;
+    private Audio audio = Audio.DEFAULT;
+
+    // -----建構子-----
+    /**
+     * 建構子，設為private，無法提供其他類別實體化。
+     */
+    private InputDialogs() {
+
+    }
+
+    // -----物件方法-----
+    /**
+     * 更改文字大小。
+     *
+     * @param font 傳入字型
+     * @param pane 傳入容器
+     */
+    private void changeFontSize(final Font font, final Pane pane) {
+        pane.getChildren().stream().forEach(node -> {
+            if (node instanceof Pane) {
+                changeFontSize(font, (Pane) node);
+            } else if (node instanceof Labeled) {
+                ((Labeled) node).setFont(font);
+            } else if (node instanceof TextField) {
+                ((TextField) node).setFont(font);
+            } else if (node instanceof ButtonBar) {
+                ((ButtonBar) node).getButtons().stream().filter(insideNode -> insideNode instanceof Button).forEach(insideNode -> {
+                    final Button button = (Button) insideNode;
+                    button.setFont(font);
+                });
+            }
+        });
+    }
+
+    /**
+     * 建立對話框。
+     *
+     * @return 傳回對話框
+     */
+    public Dialog createDialog() {
+        final Font font = new Font(fontSize);
+
+        final TextInputDialog dialog = new TextInputDialog(text);
+        dialog.initOwner(owner);
+        dialog.setTitle(title);
+        dialog.setHeaderText(header);
+        dialog.setContentText(message);
+
+        //設定標籤文字大小與高度
+        changeFontSize(font, dialog.getDialogPane());
+
+        //加入音效
+        dialog.setOnShown(e -> {
+            playAudio();
+        });
+
+        return dialog;
+    }
+
+    /**
+     * 顯示對話框並播放出音效，此方法之後的程式將會暫時停止執行，如果要取得對話框物件的參考需使用createDialog方法。
+     *
+     * @return 傳回使用者輸入的文字，如果沒有輸入，回傳null
+     */
+    public String showAndWait() {
+        final Dialog dialog = createDialog();
+        final Optional<String> buttonTypeOpt = dialog.showAndWait();
+        if (buttonTypeOpt.isPresent()) {
+            return buttonTypeOpt.get();
+        }
+        return null;
+    }
+
+    /**
+     * 設定擁有對話框的Window。
+     *
+     * @param owner 傳入擁有對話框的Window
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs owner(final Window owner) {
+        this.owner = owner;
+        return this;
+    }
+
+    /**
+     * 設定對話框的標題。
+     *
+     * @param title 傳入對話框的標題
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs title(final String title) {
+        this.title = title;
+        return this;
+    }
+
+    /**
+     * 設定對話框的訊息。
+     *
+     * @param message 傳入對話框的訊息
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs message(final String message) {
+        this.message = message;
+        return this;
+    }
+
+    /**
+     * 設定對話框的訊息標題。
+     *
+     * @param header 傳入對話框的訊息標題
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs header(final String header) {
+        this.header = header;
+        return this;
+    }
+
+    /**
+     * 設定對話框輸入方塊的預設文字內容。
+     *
+     * @param text 傳入對話框輸入方塊的預設文字內容
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs text(final String text) {
+        this.text = text;
+        return this;
+    }
+
+    /**
+     * 設定對話框的字體大小。
+     *
+     * @param fontSize 傳入對話框的字體大小
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs fontSize(final double fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
+
+    /**
+     * 設定對話框的音效，音效將在使用show或是showAndWait方法時播放出來。
+     *
+     * @param audio 傳入對話框的音效
+     * @return 再把對話框設定傳回
+     */
+    public InputDialogs audio(final Audio audio) {
+        if (audio == null) {
+            this.audio = Audio.NONE;
+        } else {
+            this.audio = audio;
+        }
+        return this;
+    }
+
+    /**
+     * 複製對話框設定。
+     *
+     * @return 傳回設定值一樣但是為不同物件的對話框設定
+     * @throws java.lang.CloneNotSupportedException
+     */
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        final InputDialogs dialogs = InputDialogs.create();
+        dialogs.audio(audio).fontSize(fontSize).owner(owner).message(message).header(header).title(title).text(text);
+        return dialogs;
+    }
+
+    /**
+     * 播放音效。
+     */
+    private void playAudio() {
+        final String audioFileName;
+        switch (audio) {
+            case NONE:
+                return;
+            case DEFAULT:
+                audioFileName = "slump.wav";
+                break;
+            default:
+                audioFileName = audio.toString().toLowerCase().concat(".wav");
+                break;
+        }
+        try {
+            final AudioPlayer ap = AudioPlayer.createPlayer(getClass().getResource("/org/magiclen/magicdialog/".concat(audioFileName)));
+            ap.setAutoClose(true);
+            ap.play();
+        } catch (final Exception ex) {
+        }
+    }
+}
